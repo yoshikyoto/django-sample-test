26fd49c41eb9cae31f6928f15e5641caf8731e29
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2020-10-04 18:30:52 +0900

    NumberTest がユニットテストになるように修正

--- projectfortest/app/number.py
+++ projectfortest/app/number.py
@@ -3,10 +3,10 @@ from app.square import Square
 class Number():
     """数字を扱うクラス"""
 
-    def __init__(self, x):
+    def __init__(self, x, square = Square()):
         self.x = x
+        self.square = square
 
     def get_square(self):
         """2乗の値を返す"""
-        square = Square()
-        return square.square(self.x)
+        return self.square.square(self.x)
--- projectfortest/app/tests.py
+++ projectfortest/app/tests.py
@@ -16,15 +16,30 @@ class SquareTest(TestCase):
         self.assertEqual(result, 9)
 
 from app.number import Number
+from unittest.mock import MagicMock
 
 class NumberTest(TestCase):
     """Number クラスのテスト"""
 
     def test_get_square(self):
-        # 2に対して get_square すると 4 になる
-        number = Number(2)
+        # モック作成
+        square_mock = MagicMock()
+
+        # square メソッドが呼ばれたら 4 を返す
+        square_mock.square.return_value = 4
+
+        # モックを注入した number オブジェクトを作成
+        number = Number(2, square=square_mock)
+
+        # get_square メソッドは、 square_mock の結果をそのまま返す
         self.assertEqual(number.get_square(), 4)
 
-        # 3に対して get_square すると 9 になる
-        number = Number(3)
-        self.assertEqual(number.get_square(), 9)
+        # square オブジェクトの square メソッドが
+        # 1回、正しく呼ばれていることを確認
+        self.assertEqual(square_mock.square.call_count, 1)
+
+        # 1回目（call_args_listt[0]）に関数が呼ばれる時の
+        #  x 引数（0番目の引数 args[0]）の値は 2
+        args, kwargs = square_mock.square.call_args_list[0]
+        self.assertEqual(args[0], 2)
+        
